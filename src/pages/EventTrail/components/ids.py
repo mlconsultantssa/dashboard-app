90a263fb8b0ee7e300f4c2589e6a6b32a869bbc8
Dylan Geldenhuys <dylan.geldenhuys@lifeq.com>
2023-03-04 20:52:05 +0800

    wip

--- src/pages/EventTrail/components/ids.py
+++
@@ -1,5 +0,0 @@
-LICENCE_PLATE_DROPDOWN = 'licence-plate-dropdown-event-trail'
-DATE_PICKER = 'date-picker-event-trail'
-MAP_GROUP_LAYER = 'map-group-layer-event-trail'
-SLIDER = 'slider-event-trail'
-CIRCLE_MARKER = 'circle-marker-event-trail'
